baa60b8da2a619ab19435d99cb30a27565a7dcdc
Jordan Reese <121062208+Jordan-Reese@users.noreply.github.com>
2023-08-09 19:05:30 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Changes to be committed:
    	modified:   netlify/functions/api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,9 +4,13 @@ var serverless = require('serverless-http');
 const api = express();
 
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+// router.get('/', (req, res) => {
+//     res.send('Hello World!');
+// })
+
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '../public/index.html'));
+});
 
 api.use('/api/', router);
 
