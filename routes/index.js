0066360864207f3f2bec48a7bb6a8d01100a156e
Jordan Reese <121062208+Jordan-Reese@users.noreply.github.com>
2023-08-09 16:15:31 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch and 'origin/main' have diverged,
     and have 2 and 9 different commits each, respectively.
      (use "git pull" to merge the remote branch into yours)
    
      Added the fs module to the project
      Added the ejs module to the project
      Added the renderTemplate.js file to the project
      Added the index.html file to the project
      Changed the index.js file to render the index.html file
    
     Changes to be committed:
    	modified:   node_modules/.package-lock.json
    	new file:   node_modules/fs/README.md
    	new file:   node_modules/fs/package.json
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   public/index.html
    	new file:   renderTemplate.js
    	modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+//added the path module for the html file
+var path = require('path');
 
-/* GET home page. */
+
+// /* GET home page. */ --- THIS IS THE CODE USED TO RENDER THE INDEX.EJS FILE
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'AVO Mobile Detailing | Auto Consultant' });
+// });
+
+
+//Added this to send the file as an html file
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AVO Mobile Detailing | Auto Consultant' });
+  res.sendFile(path.join(__dirname + '../public/index.html'));
 });
+
 
 module.exports = router;
