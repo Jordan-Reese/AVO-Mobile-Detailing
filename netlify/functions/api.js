fb9ec2b474fd3dcf98352b0f02642c6e5f613341
Jordan Reese <121062208+Jordan-Reese@users.noreply.github.com>
2023-08-16 16:54:10 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   netlify/functions/api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var serverless = require('serverless-http');
+//added the express-rate-limit module to limit the amount of requests to the server - github recommended 
+var rateLimit = require('express-rate-limit');
+const { appendFile } = require('fs');
+
+var limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+
+api.use(limiter);
 
 const api = express();
 
 const router = express.Router();
-// router.get('/', (req, res) => {
-//     res.send('Hello World!');
-// })
 
 router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '../public/index.html'));
